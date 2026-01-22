308a94d48f4a134426c2d2607a07dfb98df9ff32
Mohammad Albuainain <m.albuainain@tuta.io>
2026-01-22 21:59:33 +0300

    update Neovim Dashboard

--- nvim/.config/nvim/lua/plugins/colorscheme.lua
+++ nvim/.config/nvim/lua/plugins/colorscheme.lua
@@ -1,22 +1,29 @@
 return {
   -- Install the theme
   {
-    "scottmckendry/cyberdream.nvim",
+    "bluz71/vim-moonfly-colors",
+    name = "moonfly",
     lazy = false,
     priority = 1000,
-    opts = {
-      variant = "default",
-      transparent = true,
-      saturation = 1,
-      italic_comments = false,
-    },
+    config = function()
+      -- set any Moonfly globals you want
+      vim.g.moonflyCursorColor = true -- cursor gets theme color
+      vim.g.moonflyItalics = false -- disable italics
+      vim.g.moonflyNormalPmenu = true -- popup menu uses theme colors
+      vim.g.moonflyNormalFloat = false
+      vim.g.moonflyTerminalColors = true
+      vim.g.moonflyTransparent = false
+      vim.g.moonflyUndercurls = true
+      vim.g.moonflyUnderlineMatchParen = false
+      vim.g.moonflyVirtualTextColor = true
+      vim.g.moonflyWinSeparator = 2 -- thin separators
+    end,
   },
-
-  -- Set cyberdream as default colorscheme
+  -- Set default theme
   {
     "LazyVim/LazyVim",
     opts = {
-      colorscheme = "cyberdream",
+      colorscheme = "moonfly",
     },
   },
 }
