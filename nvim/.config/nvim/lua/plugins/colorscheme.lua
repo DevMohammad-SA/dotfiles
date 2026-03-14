d4051b8bba9d265a59a94af39fd73085535c43e5
Mohammad Albuainain <m.albuainain@tuta.io>
2026-03-15 00:23:19 +0300

    Change the default neovim theme to catppuccin

--- nvim/.config/nvim/lua/plugins/colorscheme.lua
+++ nvim/.config/nvim/lua/plugins/colorscheme.lua
@@ -1,29 +1,21 @@
 return {
-  -- Install the theme
+  -- Install Catppuccin
   {
-    "bluz71/vim-moonfly-colors",
-    name = "moonfly",
-    lazy = false,
+    "catppuccin/nvim",
+    name = "catppuccin",
     priority = 1000,
-    config = function()
-      -- set any Moonfly globals you want
-      vim.g.moonflyCursorColor = true -- cursor gets theme color
-      vim.g.moonflyItalics = false -- disable italics
-      vim.g.moonflyNormalPmenu = true -- popup menu uses theme colors
-      vim.g.moonflyNormalFloat = false
-      vim.g.moonflyTerminalColors = true
-      vim.g.moonflyTransparent = false
-      vim.g.moonflyUndercurls = true
-      vim.g.moonflyUnderlineMatchParen = false
-      vim.g.moonflyVirtualTextColor = true
-      vim.g.moonflyWinSeparator = 2 -- thin separators
-    end,
+    opts = {
+      flavour = "mocha",
+      transparent_background = false,
+      term_colors = true,
+    },
   },
-  -- Set default theme
+
+  -- Set Catppuccin as the default for LazyVim
   {
     "LazyVim/LazyVim",
     opts = {
-      colorscheme = "tokyonight-night",
+      colorscheme = "catppuccin-mocha",
     },
   },
 }
