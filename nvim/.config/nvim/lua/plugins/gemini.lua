79e1c88458bafb667b3635bdf607304fc16334a0
Mohammad Albuainain <m.albuainain@tuta.io>
2026-07-16 01:28:06 +0300

    Update 16-07-2026

--- nvim/.config/nvim/lua/plugins/gemini.lua
+++ nvim/.config/nvim/lua/plugins/gemini.lua
@@ -1,27 +1,5 @@
 return {
-  "olimorris/codecompanion.nvim",
-  dependencies = { "nvim-lua/plenary.nvim", "nvim-treesitter/nvim-treesitter" },
-  config = function()
-    require("codecompanion").setup({
-      strategies = {
-        chat = { adapter = "gemini" },
-        inline = { adapter = "gemini" },
-      },
-      adapters = {
-        gemini = function()
-          return require("codecompanion.adapters").extend("gemini", {
-            env = {
-              api_key = os.getenv("GEMINI_API_KEY"),
-            },
-            schema = {
-              model = {
-                -- We change the ACTUAL default value in the schema
-                default = "gemini-2.0-flash",
-              },
-            },
-          })
-        end,
-      },
-    })
-  end,
+  "vaijab/gemini-cli.nvim",
+  build = ":GeminiBuild",
+  opts = {},
 }
